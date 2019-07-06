77e96db81d4c7ef2de205d9c98bcea86eb0a61a7
Vladimir Chebukin <vovachebr@mail.ru>
2019-07-06 21:40:11 +0300

    Updates

---
+++ precache-manifest.8556b19caa23f493ae9c9e8667c8c8ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60d75940965f303af60a314b8633883",
+    "url": "/tariff-calculator/index.html"
+  },
+  {
+    "revision": "f1dbd02b35b7d5031508",
+    "url": "/tariff-calculator/static/css/main.0cb83c5d.chunk.css"
+  },
+  {
+    "revision": "94e4ae0ecf719b39e709",
+    "url": "/tariff-calculator/static/js/2.fd67cd4a.chunk.js"
+  },
+  {
+    "revision": "f1dbd02b35b7d5031508",
+    "url": "/tariff-calculator/static/js/main.19448404.chunk.js"
+  },
+  {
+    "revision": "2adc600a9001da515c5d",
+    "url": "/tariff-calculator/static/js/runtime~main.92dfc0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tariff-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
